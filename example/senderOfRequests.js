98bee981b7f2d8eda7294bb99fcb313e81468b99
Deve.LoL <work@deve.lol>
2022-10-12 18:28:23 +0300

    Update senderOfRequests.js

--- example/senderOfRequests.js
+++ example/senderOfRequests.js
@@ -19,21 +19,27 @@ var SenderOfRequests = /** @class */ (function () {
             ]
         ]
     });
+    // Formatting HTML text
     SenderOfRequests.textHTML = new botLite.Texter({
         markup: 'html',
         text: '<b>bold</b><br><i>italic</i><br><code>code</code><br><s>strike</s>'
     });
+    // Formatting Markdown text
     SenderOfRequests.textMarkdown = new botLite.Texter({
         markup: 'markdown',
         text: '*bold*\r\n_italic_\r\n`code`\r\n```code```\r\n~strike~'
     });
+    // Custom text
     SenderOfRequests.textCustom = 'bold\r\nitalic\r\ncode\r\ncode\r\nstrike';
+    // Object declaration for Telegram
     SenderOfRequests.requesterTelegram = new botLite.RequestTelegram({
         token: _a.configuration.tokenTelegram
     });
+    // Object declaration for VK
     SenderOfRequests.requesterVK = new botLite.RequestVK({
         token: _a.configuration.tokenVK
     });
+    // Object declaration for Viber
     SenderOfRequests.requesterViber = new botLite.RequestViber({
         token: _a.configuration.tokenViber
     });
